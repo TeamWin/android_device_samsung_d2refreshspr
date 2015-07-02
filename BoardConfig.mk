68a71bd47b9e7b590532616bc1f4e686e93cef98
Donald Hoskins <grommish@gmail.com>
2015-07-02 14:19:22 -0400

    Move outside stuff into single CM-Style repo.
    Change-Id: I370a21cd0d2edb3e0a5ed101be3a50c745b13483

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,3 +31,29 @@ TARGET_OTA_ASSERT_DEVICE := d2refreshspr
 
 TARGET_BOARD_INFO_FILE ?= device/samsung/d2refreshspr/board-info.txt
 
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=d2refreshspr \
+    TARGET_DEVICE=d2refreshspr \
+    BUILD_FINGERPRINT="samsung/d2lterefreshspr/d2lterefreshspr:4.4.2/KOT49H/L710TVPU0ANF4:user/release-keys" \
+    PRIVATE_BUILD_DESC="d2lterefreshspr-user 4.4.2 KOT49H L710TVPU0ANF4 release-keys"
+
+PRODUCT_NAME := to_d2refreshspr
+PRODUCT_DEVICE := d2refreshspr
+
+# Recovery TWRP
+RECOVERY_VARIANT := twrp
+
+TARGET_RECOVERY_FSTAB := device/samsung/d2refreshspr/twrp.fstab
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+TW_THEME := portrait_hdpi
+RECOVERY_SDCARD_ON_DATA := true
+TW_NO_REBOOT_BOOTLOADER := true
+TW_HAS_DOWNLOAD_MODE := true
+BOARD_HAS_NO_REAL_SDCARD := true
+TW_INCLUDE_CRYPTO := true
+DEVICE_RESOLUTION := 720x1280
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TARGET_USE_CUSTOM_LUN_FILE_PATH := sys/devices/virtual/android_usb/android0/f_mass_storage/lun0/file
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 zcache androidboot.selinux=permissive
+TW_DISABLE_DOUBLE_BUFFERING := true
+
